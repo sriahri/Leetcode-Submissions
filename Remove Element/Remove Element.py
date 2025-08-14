83d59715677fa27b647a85cfe16babb279403a67
Srihari Inukurthi <inukurthisrihari@gmail.com>
2025-08-14 13:03:01 -0600

    Added Remove Element

---
+++ Remove Element/Remove Element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i=0
+        while i<len(nums):
+            if nums[i]==val:
+                nums.pop(i)
+                i=0
+            else:
+                i+=1
+        return len(nums)
