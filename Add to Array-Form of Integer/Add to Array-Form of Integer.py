493249df0f8dadb73d71685ca43eec41bc7836a2
Srihari Inukurthi <inukurthisrihari@gmail.com>
2025-08-20 22:28:20 -0600

    Added Add to Array-Form of Integer

---
+++ Add to Array-Form of Integer/Add to Array-Form of Integer.py
@@ -0,0 +1,6 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        for i in range(len(num) - 1, -1, -1):
+            k, num[i] = divmod(num[i] + k, 10)
+        result = [int(i) for i in str(k)] + num if k else num
+        return result
